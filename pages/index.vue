0129e54120ffe78bbbd58390bc831277c42351af
ReaperMaga <reapermaga@gmail.com>
2024-06-02 20:21:50 +0200

    chore: Renamed Test component to SimpleText

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import SimpleText from '~/components/SimpleText.vue'
+
 const { $api } = useNuxtApp()
 
 const coffees = await $api.products.getCoffees()
@@ -6,7 +8,7 @@ const coffees = await $api.products.getCoffees()
 
 <template>
   <div class="w-screen h-screen bg-slate-950 flex items-center justify-center flex-col font-jetbrains-mono">
-    <Test v-for="coffee in coffees" :key="coffee.id" :text="coffee.title" />
+    <SimpleText v-for="coffee in coffees" :key="coffee.id" :text="coffee.title" />
   </div>
 </template>
 
